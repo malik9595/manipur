b02a22418af9b80aab5eed771c58ae25e6b7d4a4
Md Abdul Malik <mailerfakemail@gmail.com>
2024-12-16 23:09:00 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,66 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-
+import Head from "next/head";
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <>
+    <Head>
+    <title>Manipur HSLC time table 2025</title>
+    <meta name="description" content="The Manipur board releases the HSLC routine 2025 is released"/>
+    <meta name="keywords" content="boards, bosem, bsem, manipur,hslc,time,table, routine "/>
+    </Head>
+    <div className="container">  
+      <h3>PROGRAMME FOR HIGH SCHOOL LEAVING CERTIFICATE EXAMINATION, 2025</h3>
+     <article>
+     The Board of Secondary Education, Manipur has released the Manipur HSLC routine 2025 on December 16, 2024.The Manipur class 10 routine will mention the  HSLC theory and practical exam date, timing and important instructions for the students.The Manipur 10th exams for 2024 were held from March 16 to April 3, 2024. The Manipur Board declared the HSLC result for 2024 on May 27, 2024, at 3 pm. Students should regularly check this page for updates regarding the Manipur HSLC exams in 2025 and related events.
+     </article>
+     <br/>
+      <p>
+        <strong>Time:</strong> 3 hours for 80 marks & 2 hours for 48 marks
+      </p>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <div className="table">
+        <div className="row">
+          <div className="date">19-02-2025 <br/> (Wednesday)</div>
+          <div className="subject">English</div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="row">
+          <div className="date">21-02-2025 <br/> (Friday)</div>
+          <div className="subject">
+            <strong>First Language</strong> <br />
+            Manipuri (MM)/Bengali/Gangte/Hindi/Hmar/Kom/Liangmai/Mao/Mizo/Nepali/Paite/Ruangmei/Sanskrit/Simte/Tangkhul/Thadou
+            Kuki/Vaiphei/Zou/Poula/Anal/Zeme/Maram/Maring
+            <br />
+            <strong>Subjects in Lieu of First Language</strong>
+            <br />
+            Additional English/Elementary Hindi/Eman (MM)/Eman (RS)
+          </div>
+        </div>
+        <div className="row">
+          <div className="date">25-02-2025 <br/> (Tuesday)</div>
+          <div className="subject">Social Science</div>
+        </div>
+        <div className="row">
+          <div className="date">28-02-2025 <br/> (Friday)</div>
+          <div className="subject">
+            <strong>Optional Subjects </strong> <br/>
+            Home Science/Commerce/Computer Science/Higher
+            Mathematics/Thang-ta/Fine-Arts/Eshei Nongmai/Manipuri Jagoi
+          </div>
+        </div>
+        <div className="row">
+          <div className="date">04-03-2025 <br/> (Tuesday)</div>
+          <div className="subject">Science</div>
+        </div>
+        <div className="row">
+          <div className="date">07-03-2025 <br/> (Friday)</div>
+          <div className="subject">Mathematics</div>
+        </div>
+      </div>
+
+      <p className="note">
+        <strong>Note:</strong> Practical exams for subjects like Thang-ta, Fine
+        Arts, and Manipuri Jagoi will be notified later.
+      </p>
     </div>
+    </>
   );
 }
